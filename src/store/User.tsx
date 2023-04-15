f84900da5339ab7acefb4223c8f43127088a3300
王聪奕 <360805958@qq.com>
2023-04-15 16:18:05 +0800

    update png

--- src/store/User.tsx
+++ src/store/User.tsx
@@ -1,26 +1,26 @@
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import React, { createContext, Dispatch, SetStateAction, useState } from 'react'
 
 export interface UserInfo {
-    avatar: string;
-    name: string;
-    description: string;
+  avatar: string;
+  name: string;
+  description: string;
 }
 
 export interface userStoreInterface {
-    userInfo: UserInfo;
-    setUserInfo: Dispatch<SetStateAction<UserInfo>>;
+  userInfo: UserInfo;
+  setUserInfo: Dispatch<SetStateAction<UserInfo>>;
 }
 
-export const UserStore = createContext<userStoreInterface>({} as userStoreInterface);
+export const UserStore = createContext<userStoreInterface>({} as userStoreInterface)
 
 const User: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [userInfo, setUserInfo] = useState({
-        avatar: "/author.jpg",
-        name: "helianthuswhite",
-        description:
-            'Star on <a href="https://github.com/helianthuswhite/chatgpt-web-next" class="color-[#3050fb]" target="_blank" >Github</a>',
-    });
-    return <UserStore.Provider value={{ userInfo, setUserInfo }}>{children}</UserStore.Provider>;
-};
+  const [userInfo, setUserInfo] = useState({
+    avatar: '/author.png',
+    name: 'human',
+    description:
+      'Star on <a href="https://github.com/helianthuswhite/chatgpt-web-next" class="color-[#3050fb]" target="_blank" >Github</a>'
+  })
+  return <UserStore.Provider value={{ userInfo, setUserInfo }}>{children}</UserStore.Provider>
+}
 
-export default User;
+export default User
